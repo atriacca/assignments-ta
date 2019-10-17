8423fbe87ebeafbd4df77df9dfb7b13836515426
Alan Triacca <atriacca@yahoo.com>
2019-10-17 09:18:35 -0600

    minor updates to resume and portfolio

--- projects/atriacca-portfolio/src/Profile.js
+++ projects/atriacca-portfolio/src/Profile.js
@@ -4,7 +4,7 @@ function Profile() {
     return (
         <div>
             <p>Hi! My name is Alan Triacca and I am a full stack software developer based in Utah.</p>
-            <p>I'm just as passionate about making things as I am with improving everything I see and just finding ways to make the planet a better place for everyone.</p>
+            <p>I'm just as passionate about making things as I am with improving everything I see. I just love finding ways to make the planet a better place for everyone.</p>
             <p>After graduating from the University of Connecticut, I spent more than ten years with two different companies. First in the semiconductor industry, mostly as an Applications Engineer and Account Manager. Then in investor education, as a Client Service/Technical Support specialist with significant time in Tier 2 or supervisory roles and as a liaison with development. Both employers frequently had me serve on cross-functional teams
             {/* as this is where I shine brightest */}
             .</p>
