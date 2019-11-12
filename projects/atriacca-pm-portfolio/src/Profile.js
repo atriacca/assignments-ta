94d85f81cbb85f93f8f9d8f30abeee1497ad5e18
Alan Triacca <atriacca@yahoo.com>
2019-11-11 23:04:33 -0700

    update pm-portfolio profile and resume

--- projects/atriacca-pm-portfolio/src/Profile.js
+++ projects/atriacca-pm-portfolio/src/Profile.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Profile() {
     return (
         <div>
-            <p>Hi! My name is Alan Triacca and my greatest strengths are blending the talents of diverse individuals and facilliating cross-functional teams to deliver phenomenal results. I'm based in Utah.</p>
-            <p>I'm just as passionate to improve anything I see as I am about working with others to deliver amazing solutions to any situation presented to us. I just love finding ways to make the planet a better place for everyone.</p>
+            <p>Hi! My name is Alan Triacca and among my greatest strengths is blending the talents of diverse individuals and facilitating cross-functional teams to deliver phenomenal results. I'm based in Utah.</p>
+            {/* <p>I'm just as passionate about improving anything I see as I am about working with others to deliver amazing solutions to any situation presented. I just love finding ways to make the planet a better place for everyone.</p> */}
             <p>After graduating from the University of Connecticut, I spent more than ten years with two different companies. First in the semiconductor industry, mostly as an Applications Engineer and Account Manager. Then in investor education, as a Client Service/Technical Support specialist with significant time in Tier 2 and supervisory roles as well as being a liaison with development. Both employers frequently had me serve on a variety of cross-functional teams as this is where I shine brightest.</p>
             <p>I completed the Full Stack JavaScript Web Development Bootcamp at V School in Salt Lake City in May 2019. I was also a Teaching Assistant at V School from May to September 2019. I'm very excited to embark on my new career!</p>
         </div>
